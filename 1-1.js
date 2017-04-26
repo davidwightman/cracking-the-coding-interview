4b799ab8b2f457bfccd482ad03ae8799fee91e32
wightman <davidwightmanmusic@gmail.com>
2017-04-26 10:03:27 -0400

    deleted comments

--- 1-1.js
+++ 1-1.js
@@ -1,15 +1,9 @@
-/*
-     * Tests to see if string contains all unique chars.
-     * @param {String} str - The string to be checked for uniqueness
-     * @returns {Boolean} true if string has only unique chars. False if a duplicate exists
-     */
-
     var str1 = "helol"
     var str2 = "jdsgg"
 
     var isUnique = function(string) {
     	var stringArray = Array.from(string).sort();
-    	if (stringArray.length > 256) return false;
+    	if (stringArray.length > 256) return false;ubl
     	for (i = 1; i <= string.length; i++){
     		console.log(i)
     		if (stringArray[i] === stringArray[i - 1]) {
